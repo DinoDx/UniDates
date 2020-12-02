13180988ae2e13a9465e5541be854e71791e64b7
Alessandro <a.dantuono35@studenti.unisa.it>
2020-12-02 21:48:04 +0100

    Aggiunta cartella di sviluppo

---
+++ UniDates/src/test/java/com/unisa/UniDates/UniDatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisa.UniDates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniDatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
