e74f006c71f6f97232d54458c4243d4978c6c887
axel1143 <44553513+axel1143@users.noreply.github.com>
2020-12-03 12:07:49 +0100

    aggiunta cartella springIntz

---
+++ UniDates/src/main/java/com/unidates/Unidates/UniDates/UniDatesApplication.java
@@ -0,0 +1,13 @@
+package com.unidates.Unidates.UniDates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniDatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniDatesApplication.class, args);
+	}
+
+}
---
+++ UniDates/src/test/java/com/unidates/Unidates/UniDates/UniDatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unidates.Unidates.UniDates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniDatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
