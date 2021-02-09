a6515de01b97ad376568b31d8daa3a37b1c299e4
roberta94g <64490865+roberta94g@users.noreply.github.com>
2021-02-09 20:48:54 +0100

    TestNotificaManager

--- UniDates/src/test/java/com/unidates/Unidates/UniDates/Unit/TestNotificaManager.java
+++ UniDates/src/test/java/com/unidates/Unidates/UniDates/Unit/TestNotificaManager.java
@@ -3,6 +3,7 @@ package com.unidates.Unidates.UniDates.Unit;
 import com.unidates.Unidates.UniDates.Exception.EntityNotFoundException;
 import com.unidates.Unidates.UniDates.Manager.NotificaManager;
 import com.unidates.Unidates.UniDates.Model.Entity.*;
+import com.unidates.Unidates.UniDates.Model.Enum.*;
 import com.unidates.Unidates.UniDates.Repository.AmmonimentiRepository;
 import com.unidates.Unidates.UniDates.Repository.FotoRepository;
 import com.unidates.Unidates.UniDates.Repository.NotificaRepository;
@@ -19,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 @SpringBootTest
@@ -39,91 +41,135 @@ public class TestNotificaManager {
     @MockBean
     AmmonimentiRepository ammonimentiRepository;
 
+
     @Test
-    public void generateNotificaMatch_valid(){
+    public void generateNotificaMatch_valid1(){
+        Mockito.when(utenteRepository.findByEmail(anyString())).thenAnswer(invocationOnMock -> {
+            Studente s= new Studente(invocationOnMock.getArgument(0, String.class), "password");
 
-        Studente s= new Studente();
-        s.setEmail("pippo");
-        Profilo p= new Profilo();
-        p.setNome("pippo");
-        ArrayList<Foto> foto= new ArrayList<>();
-        Foto f=new Foto();
-        f.setFotoProfilo(true);
-        foto.add(f);
-        p.setListaFoto(foto);
-        s.setProfilo(p);
+            ArrayList<Hobby> hobbyArrayList = new ArrayList<Hobby>();
 
-        Mockito.when(utenteRepository.findByEmail(anyString())).thenReturn(s);
+            hobbyArrayList.add(Hobby.ARTE);
+            hobbyArrayList.add(Hobby.ANIME);
+            hobbyArrayList.add(Hobby.CALCIO);
 
-        assertDoesNotThrow(() -> notificaManager.generateNotificaMatch("pippo", "pluto"));
+            Profilo p = new Profilo("Marco", "Prova1", "Napoli", "Napoli", LocalDate.of(1999,2,10), 170, Sesso.UOMO, Interessi.DONNE, Colori_Capelli.AMBRA, Colore_Occhi.AZZURRI,new Foto() ,hobbyArrayList);
+            s.setProfilo(p);
+            return s;
+        });
 
+        assertDoesNotThrow(() -> notificaManager.generateNotificaMatch("pippo@gmail.com", "pluto@gmail.com"));
     }
+
 
     @Test
     public void generateNotificaMatch_NotValid(){
         Mockito.when(utenteRepository.findByEmail(anyString())).thenReturn(null);
-        assertThrows(EntityNotFoundException.class, () -> notificaManager.generateNotificaMatch("ciao","ciao"));
+        assertThrows(EntityNotFoundException.class, () -> notificaManager.generateNotificaMatch("ciao@gmail.com","hello@gmail.com"));
     }
 
-    //controllare
+
     @Test
     public void eliminaNotificaMatch_valid() {
-        Mockito.when(utenteRepository.findByEmail(anyString())).thenReturn(new Studente());
-        Mockito.when(notificaRepository.findByUtenteAndEmailToMatchWith(any(Studente.class),anyString())).thenAnswer(invocation ->{
-            return new Notifica();
+        Mockito.when(utenteRepository.findByEmail(anyString())).thenAnswer(invocationOnMock -> {
+            Studente s= new Studente(invocationOnMock.getArgument(0, String.class), "password");
+            return s;
         });
-        assertDoesNotThrow(() -> notificaManager.eliminaNotificaMatch("pippo", "pluto"));
+
+        Mockito.when(notificaRepository.findByUtenteAndEmailToMatchWith(any(Studente.class),anyString())).thenAnswer(invocationOnMock -> {
+           byte[]img= {1,2,3};
+            Foto s=new Foto(img);
+            s.setId(1L);
+            Notifica n= new Notifica(invocationOnMock.getArgument(0, Studente.class), "testoNotifica", Tipo_Notifica.MATCH, s);
+            return n;
+        });
+        assertDoesNotThrow(() -> notificaManager.eliminaNotificaMatch("pippo@gmail.com", "pluto@gmail.com"));
 
     }
 
     @Test
     public void eliminaNotificaMatch_NotValidUtente(){
         Mockito.when(utenteRepository.findByEmail(anyString())).thenReturn(null);
-        assertThrows(EntityNotFoundException.class, () -> notificaManager.eliminaNotificaMatch("pippo", "pluto"));
+        assertThrows(EntityNotFoundException.class, () -> notificaManager.eliminaNotificaMatch("pippo@gmail.com", "pluto@gmail.com"));
     }
 
 
     @Test
     public void eliminaNotificaMatch_NotValidNotifica(){
-        Mockito.when(utenteRepository.findByEmail(anyString())).thenReturn(new Studente());
+        Mockito.when(utenteRepository.findByEmail(anyString())).thenAnswer(invocationOnMock -> {
+            Studente s=new Studente(invocationOnMock.getArgument(0,String.class), "password");
+            return s;
+        });
         Mockito.when(notificaRepository.findByUtenteAndEmailToMatchWith(any(Utente.class),anyString())).thenReturn(null);
-        assertThrows(EntityNotFoundException.class, () -> notificaManager.eliminaNotificaMatch("pippo", "pluto"));
+        assertThrows(EntityNotFoundException.class, () -> notificaManager.eliminaNotificaMatch("pippo@gmail.com", "pluto@gmail.com"));
     }
 
     @Test
     public void generateNotificaWarning_valid(){
-        Mockito.when(utenteRepository.findByEmail(anyString())).thenAnswer(invocation ->{
-            Studente s= new Studente();
-            Profilo p= new Profilo();
-            p.setNome("pippo");
+        Mockito.when(utenteRepository.findByEmail(anyString())).thenAnswer(invocation -> {
+            Studente s= new Studente(invocation.getArgument(0, String.class), "password");
+
+            ArrayList<Hobby> hobbyArrayList = new ArrayList<Hobby>();
+
+            hobbyArrayList.add(Hobby.ARTE);
+            hobbyArrayList.add(Hobby.ANIME);
+            hobbyArrayList.add(Hobby.CALCIO);
+
+            Profilo p = new Profilo("Marco", "Prova1", "Napoli", "Napoli", LocalDate.of(1999,2,10), 170, Sesso.UOMO, Interessi.DONNE, Colori_Capelli.AMBRA, Colore_Occhi.AZZURRI,new Foto() ,hobbyArrayList);
             s.setProfilo(p);
             return s;
+
         });
-        Mockito.when(fotoRepository.findFotoById(anyLong())).thenReturn(new Foto());
-        Mockito.when(ammonimentiRepository.findByFoto(any(Foto.class))).thenReturn(new Ammonimento());
-        assertDoesNotThrow(() -> notificaManager.genereateNotificaWarning("pippo",Long.valueOf(1L)));
+
+        Mockito.when(fotoRepository.findFotoById(anyLong())).thenAnswer(invocationOnMock -> {
+            byte[]img= {1,2,3};
+            Foto f=new Foto(img);
+            f.setId(invocationOnMock.getArgument(0, Long.class));
+            return f;
+        });
+
+        Mockito.when(ammonimentiRepository.findByFoto(any(Foto.class))).thenAnswer(invocationOnMock -> {
+            Ammonimento a= new Ammonimento(Motivazione.VIOLENZA, "messaggio");
+            a.setFoto(invocationOnMock.getArgument(0, Foto.class));
+            return a;
+        });
+        assertDoesNotThrow(() -> notificaManager.genereateNotificaWarning("pippo@gmail.com",Long.valueOf(1L)));
 
     }
 
     @Test
     public void generateNotificaWarning_NotValidStudente(){
         Mockito.when(utenteRepository.findByEmail(anyString())).thenReturn(null);
-        assertThrows(EntityNotFoundException.class, () -> notificaManager.genereateNotificaWarning("pippo", 1L));
+        assertThrows(EntityNotFoundException.class, () -> notificaManager.genereateNotificaWarning("pippo@gmail.com", 1L));
     }
 
     @Test
     public void generateNotificaWarning_NotValidAmmonimento(){
-        Mockito.when(utenteRepository.findByEmail(anyString())).thenReturn(new Studente());
-        Mockito.when(fotoRepository.findFotoById(anyLong())).thenReturn(new Foto());
+        Mockito.when(utenteRepository.findByEmail(anyString())).thenAnswer(invocationOnMock -> {
+            Studente s=new Studente(invocationOnMock.getArgument(0,String.class), "password");
+            return s;
+        });
+
+        Mockito.when(fotoRepository.findFotoById(anyLong())).thenAnswer(invocationOnMock -> {
+        byte[]img= {1,2,3};
+        Foto f=new Foto(img);
+        f.setId(invocationOnMock.getArgument(0, Long.class));
+        return f;
+        });
+
         Mockito.when(ammonimentiRepository.findByFoto(any(Foto.class))).thenReturn(null);
-        assertThrows(EntityNotFoundException.class, () -> notificaManager.genereateNotificaWarning("pluto", 1L));
+        assertThrows(EntityNotFoundException.class, () -> notificaManager.genereateNotificaWarning("pluto@gmail.com", 1L));
     }
 
     @Test
     public void generateNotificaWarning_NotValidFoto(){
-        Mockito.when(utenteRepository.findByEmail(anyString())).thenReturn(new Studente());
+        Mockito.when(utenteRepository.findByEmail(anyString())).thenAnswer(invocationOnMock -> {
+            Studente s= new Studente(invocationOnMock.getArgument(0, String.class), "password");
+            return s;
+        });
+
         Mockito.when(fotoRepository.findFotoById(anyLong())).thenReturn(null);
-        assertThrows(EntityNotFoundException.class, () -> notificaManager.genereateNotificaWarning("pluto", 2L));
+        assertThrows(EntityNotFoundException.class, () -> notificaManager.genereateNotificaWarning("pluto@gmail.com", 2L));
 
     }
 
