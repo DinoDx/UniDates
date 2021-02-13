7ad580b01c18b42d16cf9fe83e39418cc2a41ba3
alexp <alexpd1143@gmail.com>
2021-02-13 20:20:55 +0100

    aggiustato test registrazione

--- UniDates/src/test/java/com/unidates/Unidates/UniDates/Unit/TestOnRegistrationCompleteEvent.java
+++ UniDates/src/test/java/com/unidates/Unidates/UniDates/Unit/TestOnRegistrationCompleteEvent.java
@@ -2,9 +2,14 @@ package com.unidates.Unidates.UniDates.Unit;
 
 import com.unidates.Unidates.UniDates.Manager.Registrazione.ConfermaRegistrazione;
 import com.unidates.Unidates.UniDates.Manager.Registrazione.OnRegistrationCompleteEvent;
+import com.unidates.Unidates.UniDates.Manager.UserManager;
 import com.unidates.Unidates.UniDates.Model.Entity.Studente;
+import com.unidates.Unidates.UniDates.Model.Entity.Utente;
+import com.unidates.Unidates.UniDates.Model.Entity.VerificationToken;
+import com.unidates.Unidates.UniDates.Repository.VerificationTokenRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,6 +19,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import java.util.Locale;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 
 @SpringBootTest
@@ -25,9 +31,21 @@ public class TestOnRegistrationCompleteEvent {
     @Autowired
     private ConfermaRegistrazione confermaRegistrazione;
 
+    @Autowired
+    private VerificationTokenRepository verificationTokenRepository;
+
+    @MockBean
+    private UserManager userManager;
+
     @Test
     public void launchEvent_valid() {
 
+        Mockito.when(userManager.createVerificationToken(any(Utente.class),  anyString())).thenAnswer(invocation -> {
+            VerificationToken verificationToken = new VerificationToken();
+            verificationToken.setUtente(invocation.getArgument(0, Utente.class));
+            verificationToken.setToken(invocation.getArgument(1, String.class));
+            return verificationToken;
+        });
         Studente utente = new Studente("testregistrazione@gmail.com", "passwordp");
         OnRegistrationCompleteEvent event = new OnRegistrationCompleteEvent(utente, Locale.getDefault(), "");
         doNothing().when(javaMailSender).send(any(SimpleMailMessage.class));
@@ -35,6 +53,7 @@ public class TestOnRegistrationCompleteEvent {
         oracolo.setTo(utente.getEmail());
         oracolo.setText("http://localhost:8080" + event.getAppUrl() + "/api/UserManager/registrationConfirm?token=");
         oracolo.setSubject("Conferma la registrazione");
+
         SimpleMailMessage ritornato = confermaRegistrazione.confermaRegistrazione(event);
         ritornato.setText(ritornato.getText().substring(0, ritornato.getText().length() - 36));
         Assertions.assertEquals(oracolo, ritornato);
